5577c3670665a30c789ab22813648e9e475ebc71
Kenny Durkin <kenneth.durkin@mtahq.org>
2025-11-06 13:22:05 -0500

    Force adds the lib directory

---
+++ lib/typescript/plugin/build/withMapbox.d.ts
@@ -0,0 +1,26 @@
+export const __esModule: boolean;
+declare const _default: config_plugins_1.ConfigPlugin<{
+    RNMapboxMapsImpl: any;
+    RNMapboxMapsVersion: any;
+    RNMapboxMapsDownloadToken: any;
+    RNMapboxMapsUseV11: any;
+}>;
+export default _default;
+export function addInstallerBlock(src: any, blockName: any): any;
+export function addConstantBlock(src: any, { RNMapboxMapsImpl, RNMapboxMapsVersion, RNMapboxMapsDownloadToken, RNMapboxMapsUseV11, }: {
+    RNMapboxMapsImpl: any;
+    RNMapboxMapsVersion: any;
+    RNMapboxMapsDownloadToken: any;
+    RNMapboxMapsUseV11: any;
+}): any;
+export function applyCocoaPodsModifications(contents: any, { RNMapboxMapsImpl, RNMapboxMapsVersion, RNMapboxMapsDownloadToken, RNMapboxMapsUseV11, }: {
+    RNMapboxMapsImpl: any;
+    RNMapboxMapsVersion: any;
+    RNMapboxMapsDownloadToken: any;
+    RNMapboxMapsUseV11: any;
+}): any;
+export function addMapboxInstallerBlock(src: any, blockName: any): string;
+export function addMapboxMavenRepo(src: any): any;
+import config_plugins_1 = require("expo/config-plugins");
+export { addMapboxMavenRepo as _addMapboxMavenRepo };
+//# sourceMappingURL=withMapbox.d.ts.map
